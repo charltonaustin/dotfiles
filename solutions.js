2fbf096d151068b0ee768dae408fbaab9c36e4bb
charltonaustin <charlton.austin@gmail.com>
2016-05-24 10:07:13 +0300

    Adding in a solutions file to share with Pavel.

---
+++ solutions.js
@@ -0,0 +1,18 @@
+var a = "cat";
+var arr = a;
+a = "dog"
+console.log("a: " + a)
+console.log("arr: " + arr)
+
+var a = ["cat"];
+var arr = a;
+a[0] = "dog"
+console.log("a[0]: " + a[0])
+console.log("arr[0]: " + arr[0])
+
+var a = {animal: "cat"};
+var arr = a;
+a.animal = "dog"
+console.log("a.animal: " + a.animal)
+console.log("arr.animal: " + arr.animal)
+
